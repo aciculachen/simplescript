273d9d5ad559cc6e5371536830db7d5852d1ae6c
aciculachen <am3000520@gmail.com>
2017-03-02 13:04:31 +0800

    Update add.sh

--- add.sh
+++ add.sh
@@ -7,8 +7,9 @@ passwd="farnwang"
 echo "user is $username"
 echo ""password is $passwd
 
-sudo adduser --force-badname "$username"
+echo "$passwd
+$passwd" | adduser --force-badname "$username"
 
-echo $passwd | passwd --stdin $username
+
 
  
