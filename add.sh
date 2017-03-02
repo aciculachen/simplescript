872b40ac3c413e60d5370a06b4aa54a723256f62
aciculachen <am3000520@gmail.com>
2017-03-02 10:57:59 +0800

    Update add.sh

--- add.sh
+++ add.sh
@@ -3,17 +3,6 @@
 user = VAL618
 passwd = farnwang
 
-if [ $(id -u) -eq 0 ]; then
-	egrep "^$user" /etc/passwd >/dev/null
-	if [ $? -eq 0 ]; then
-		echo "$user exists!"
-		exit 1
-	else
-		pass=$(perl -e 'print crypt($ARGV[0], "password")' $passwd)
-		useradd -m -p $passwd $user
-		[ $? -eq 0 ] && echo "User has been added to system!" || echo "Failed to add a user!"
-	fi
-else
-	echo "Only root may add a user to the system"
-	exit 2
-fi
+adduser "$user"
+echo "$passwd" | passwd "$user" --stdin
+
